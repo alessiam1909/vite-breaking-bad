51b27d4ab93e264a7f85354e5f216389f606cf32
alessiam1909 <ale.menon.vr@gmail.com>
2023-01-13 14:40:18 +0100

    creazione repo e inizio collegamento file

---
+++ src/components/AppContent.vue
@@ -0,0 +1,29 @@
+<script>
+
+import {store} from '../store.js'
+export default {
+    data(){
+        return{
+            store,
+        }
+    },
+    created(){
+        getElementApi(){
+            axios.get(' https://db.ygoprodeck.com/api/v7/cardinfo.php').then((risultato =>{
+                cardList = risultato.data.response
+            }))
+        }
+    }
+}
+</script>
+
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<style lang="scss">
+    @use './assets/styles/partials/_variables.scss' as *; 
+    @use './assets/styles/partials/_mixins.scss' as *; 
+</style>
