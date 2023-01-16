752cea11f11e3f5cddd0d2f21526fc68e121d8cf
alessiam1909 <ale.menon.vr@gmail.com>
2023-01-16 18:00:35 +0100

    sistemazione header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,6 +7,8 @@ export default {
 <template lang="">
     <header>
         <div class="container">
+            <img class="logo" src="../assets/ygoprodeck_header_logo.png" alt="ygoprodeck.com Logo">
+        
             <div class="titolo">
                 <h2>Yu-Gi-Oh API</h2>
             </div>
@@ -18,5 +20,18 @@ export default {
     @use '../assets/styles/partials/variables' as *;
     header{
         background-color: white;
+
+        .container{
+            display: flex;
+            width: 90%;
+            margin: 0 auto;
+            padding: 30px 0;
+
+            .logo{
+                margin: 0 30px;
+            }
+
+           
+        }
     }
 </style>
